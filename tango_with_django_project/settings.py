224fda5dae28abceb65d16384fb281de1155df35
“Charles <2896588b@student.gla.ac.uk>
2025-02-14 03:38:34 +0000

    Chapter 9 commit after tests

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,6 +25,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '21gcf#!kj$qp$zh$8%v(%bl0u!!5@vpxvq%$_n2f-p$v($l1j+'
 
+LOGIN_URL = 'rango:login'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
